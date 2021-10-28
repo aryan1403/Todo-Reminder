3035acc2f32e7e721ab0b949f7ea0b733724910e
Aaryan <aaryan14032006@gmail.com>
2021-10-28 20:47:35 +0530

    created Todo.dart for creating TODOS

---
+++ lib/Components/todo.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+createTodo(String todo) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 10, right: 10),
+    child: ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Card(
+        shadowColor: Colors.black,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(
+                Icons.today_outlined,
+                color: Colors.red,
+              ),
+              title: Text(
+                todo,
+                style: TextStyle(fontSize: 22),
+              ),
+              subtitle: Text('Till : 8pm'),
+              trailing: Icon(
+                Icons.pending_actions,
+                color: Colors.black,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text(
+                    'Done',
+                    style: TextStyle(color: Colors.green, fontSize: 17),
+                  ),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('Snooze', style: TextStyle(fontSize: 17)),
+                  onPressed: () {},
+                ),
+                const SizedBox(width: 8),
+              ],
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
