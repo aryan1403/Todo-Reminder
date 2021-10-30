9ba43a9c2080ecd34763757b60da299b33b07f19
Aaryan <aaryan14032006@gmail.com>
2021-10-30 21:43:16 +0530

    Initialiazed sharedPrefs

--- lib/App.dart
+++ lib/App.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_reminder/Components/appBar.dart';
 import 'package:todo_reminder/Components/todo.dart';
+import 'package:todo_reminder/DB/sharedPrefs.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -13,12 +14,33 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            appBar(),
-            createTodo('Learn Java'),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          // color: Colors.black,
+          child: Column(
+            children: [
+              appBar(),
+              createTodo('Learn Java'),
+              createTodo('Learn Spring'),
+              createTodo('Learn Node'),
+              createTodo('Learn Express'),
+              createTodo('Learn Angular'),
+              createTodo('Learn React'),
+              //SharedPrefs.addTodo('Meow'),
+              FutureBuilder<dynamic>(
+                future: SharedPrefs.getAllTodo(), // async work
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return createTodo(snapshot.data);
+                  } else if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  } else {
+                    return Text('No Data Found!');
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
